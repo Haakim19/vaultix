e605a0a13e29165e56ba751b41edc30db42afb7c
Haakim19 <haakimajmal7@gmail.com>
2026-09-09 19:02:12 +0530

    sample code to test the application

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+
+
+def load_ui(filename):
+    loader = QUiLoader()
+    ui_file = QFile(filename)
+
+    ui_file.open(QFile.ReadOnly)
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    return window
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = QLabel("Vaultix")
-    window.resize(400, 200)
+    window = load_ui("ui/login.ui")
+    
+    window.unlockButton.clicked.connect(
+        lambda : show_message(window)
+    )
     window.show()
 
-    return app.exec()
+    sys.exit(app.exec())
 
-
+def show_message(window):
+    QMessageBox.information(
+        window,
+        "Vaultix",
+        "Unlock Button Clicked"
+    )
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
