8715971df58e28a644b4ed8ee9d2e10352004b28
Haakim19 <haakimajmal7@gmail.com>
2026-09-10 17:55:50 +0530

    update with vault creation

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMessageBox
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PySide6.QtWidgets import QApplication
 
-
-def load_ui(filename):
-    loader = QUiLoader()
-    ui_file = QFile(filename)
-
-    ui_file.open(QFile.ReadOnly)
-    window = loader.load(ui_file)
-    ui_file.close()
-
-    return window
+from app.views.create_vault import create_vault
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = load_ui("ui/login.ui")
-    
-    window.unlockButton.clicked.connect(
-        lambda : show_message(window)
-    )
+    window = create_vault()
     window.show()
 
     sys.exit(app.exec())
 
-def show_message(window):
-    QMessageBox.information(
-        window,
-        "Vaultix",
-        "Unlock Button Clicked"
-    )
+
 if __name__ == "__main__":
     main()
