a90e4cf157d300f5cbaac49caab215f5fb345377
Haakim19 <haakimajmal7@gmail.com>
2026-09-12 12:18:31 +0530

    message box update, connceted to vault creating

--- app/views/create_vault.py
+++ app/views/create_vault.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QMessageBox
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
-
+from app.services.vault_service import create_vault
 
 def load_ui(filename):
     loader = QUiLoader()
@@ -14,10 +14,13 @@ def load_ui(filename):
     return None
 
 
-def create_vault():
+def main():
 
     window = load_ui("ui/create_vault.ui")
 
+    if window is None:
+        raise RuntimeError("Failed to load UI file: ui/create_vault.ui")
+    
     window.createVaultButton.clicked.connect(
         lambda: validate_data(window)
     )
@@ -34,23 +37,29 @@ def validate_data(window):
     error =(
         "Vault name is empty" if not vault_name.strip() else
         "Password is empty" if not password else
-        "Password lenght is not 8 charecters" if len(password) < 8 else
+        "Password must be at least 8 charecters" if len(password) < 8 else
         "Confirm password is empty" if not confirm_password else
-        "Password is not same" if confirm_password != password else None
+        "Password is not same" if confirm_password != password else 
+        None
     )
     if error:
-        show_message(window, error)
+        show_message(window, error, is_error = True)
         return
-        
-    show_message(window, "Validation successful")
-    clear_fields(window)
+    # Save to Database 
+    try:
+        created_vault = create_vault(vault_name, password)
+        # show success message 
+        show_message(window, f"Vault '{created_vault.name}' created successfully!")
+        clear_fields(window)
+    except Exception as e:
+        # Handle database error gracfully
+        show_message(window, f"Faild to create vault {e}", is_error= True)
 
-def show_message(window, message):
-    QMessageBox.information(
-        window,
-        "Vaultix",
-        message
-    )
+def show_message(window, message, is_error = False):
+    if is_error:
+        QMessageBox.warning(window, "Vaultix", message)
+    else:
+        QMessageBox.information(window, "Vaultix", message)
 
 def clear_fields(window):
     window.vaultNameInput.clear()
