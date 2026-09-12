00e00d18b6c283ce8c44d7c172997e145f6551b1
Haakim19 <haakimajmal7@gmail.com>
2026-09-12 17:21:28 +0530

    function name update

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from app.views.create_vault import create_vault
+from app.views.create_vault import main_vault_creation
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = create_vault()
+    window = main_vault_creation()
     window.show()
 
     sys.exit(app.exec())
