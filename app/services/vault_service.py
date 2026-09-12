4c4345ca2da3843c693f75b1add0a814287a5f0a
Haakim19 <haakimajmal7@gmail.com>
2026-09-12 12:19:13 +0530

    vault creation code added

---
+++ app/services/vault_service.py
@@ -0,0 +1,39 @@
+from app.security.crypto import(
+    generate_salt,
+    derive_key,
+    encrypt_data
+)
+from datetime import datetime, timezone
+from app.database.database import SessionLocal
+from app.models.models import Vault
+
+def create_vault(vault_name, master_password):
+    # generate the salt
+    salt = generate_salt()
+    
+    # generate vault key
+    vault_key = derive_key(master_password, salt)
+    
+    # encrypting the verification data using the vault key
+    ciphertext, nonce = encrypt_data(
+        "VAULTIX_VERIFICATION",
+        vault_key
+    )
+    
+    vault = Vault(
+        name = vault_name,
+        salt = salt,
+        encrypted_verification = ciphertext,
+        verification_nonce = nonce
+    )
+    
+    with SessionLocal() as db:
+        try:
+            db.add(vault)
+            db.commit()
+            db.refresh(vault)
+            db.expunge(vault)
+            return vault
+        except Exception:
+            db.rollback()
+            raise
