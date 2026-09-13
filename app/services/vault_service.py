4a1d82d7ca331fdfd6a8c88d650bdc9e956a031e
Haakim19 <haakimajmal7@gmail.com>
2026-09-13 12:41:55 +0530

    get vault & vault by id

--- app/services/vault_service.py
+++ app/services/vault_service.py
@@ -1,4 +1,5 @@
 from app.models.models import Vault
+from sqlalchemy import select
 from app.security.crypto import(
     generate_salt,
     derive_key,
@@ -49,3 +50,15 @@ def unlock_vault(vault, master_password):
         return decrypted_data == "VAULTIX_VERIFICATION"
     except Exception:
         return False
+
+def get_vaults(db):
+        vaults = db.execute(select(Vault).order_by(Vault.vault_id)).scalars().all()
+        for vault in vaults:
+            db.expunge(vault)
+        return vaults
+
+def get_vault_by_id(vault_id, db):
+    result = db.execute(
+        select(Vault).where(Vault.vault_id == vault_id)
+    )
+    return result.scalar_one_or_none()
