872cf480ec7a28e6578d91b201ac175ce6e4e519
Haakim19 <haakimajmal7@gmail.com>
2026-09-13 12:40:44 +0530

    update

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from app.views.create_vault import main_vault_creation
+from app.views.login import login_window
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = main_vault_creation()
+    window = login_window()
     window.show()
 
     sys.exit(app.exec())
