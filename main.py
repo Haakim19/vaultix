d7da753db795dd413176b21d425a47ed0bc14045
Haakim19 <haakimajmal7@gmail.com>
2026-09-14 21:57:22 +0530

    set to open the window accoringly with vault existence

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
+from app.database.database import SessionLocal
+from app.services.vault_service import any_vault_exist
+
 from app.views.create_vault import main_vault_creation
 from app.views.login import login_window
 
@@ -9,11 +12,17 @@ from app.views.login import login_window
 def main():
     app = QApplication(sys.argv)
 
-    window = login_window()
+    with SessionLocal() as db:
+        has_vault = any_vault_exist(db)
+    
+    if has_vault:
+        window = login_window()
+    else:
+        window = main_vault_creation()
     window.show()
 
     sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
