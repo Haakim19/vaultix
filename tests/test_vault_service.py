c2f4c3215a4adb11545655a7e8910f84c347a7e9
Haakim19 <haakimajmal7@gmail.com>
2026-09-14 21:59:14 +0530

    update

--- tests/test_vault_service.py
+++ tests/test_vault_service.py
@@ -17,24 +17,29 @@ password = "12345678"
 
 def test_create_vault():
     with testSession() as db:
-        created_test_vault = create_vault(vault_name, password, db)
+        created_test_vault, vault_key = create_vault(db, vault_name, password)
     print(f"vault '{vault_name}' created")
 
     found_vault = get_vault_by_id(
-        created_test_vault.vault_id,
-        db
+        db,
+        created_test_vault.vault_id
     )
     
     assert found_vault is not None
+    assert isinstance(vault_key, bytes)
+    assert len(vault_key) == 32
     assert found_vault.name == vault_name
     print(f"✅ Found the vault '{vault_name}'")
 
     result = unlock_vault(created_test_vault, password)
-    assert result is True
+
+    assert result is not None
+    assert isinstance(result, bytes)
+    assert len(result) == 32
     print("✅ Correct password: vault unlocked")
 
     wrong_result = unlock_vault(created_test_vault, "123456789")
-    assert wrong_result is False
+    assert wrong_result is None
     print("✅ Wrong Password: vault not-unlocked")
     
 if __name__ == "__main__":
