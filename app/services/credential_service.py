75211c22154e4bc8119a5c2bb53c1096191fc471
Haakim19 <haakimajmal7@gmail.com>
2026-09-15 21:04:31 +0530

    get, decrypt, update credentials function added

--- app/services/credential_service.py
+++ app/services/credential_service.py
@@ -1,5 +1,7 @@
 from app.models.models import Credential
-from app.security.crypto import encrypt_data
+from app.security.crypto import encrypt_data, decrypt_data
+from sqlalchemy import select
+
 
 def add_credentials(
     db,
@@ -21,7 +23,7 @@ def add_credentials(
     credential = Credential(
         vault_id = session.vault.vault_id,
         category_id = category_id,
-        title = title.srip(),
+        title = title.strip(),
         website = website.strip() if website else None,
         encrypted_username = encrypted_username,
         username_nonce = username_nonce,
@@ -35,5 +37,74 @@ def add_credentials(
     return credential
 
 
-def get_credentials():
-    pass
+
+def get_credentials(db, session):
+    credentials = db.execute(
+        select(Credential).where(
+            Credential.vault_id == session.vault.vault_id)
+    ).scalars().all()
+    for credential in credentials:
+        db.expunge(credential)
+    return credentials
+
+
+def decrypt_credential(credential: Credential, session):
+    username = decrypt_data(
+        credential.encrypted_username,
+        credential.username_nonce,
+        session.vault_key)
+    
+    password = decrypt_data(
+        credential.encrypted_password,
+        credential.password_nonce,
+        session.vault_key
+    )
+    return username, password
+
+def update_credential(
+    db, 
+    session,
+    credential: Credential, 
+    title,
+    website,
+    username,
+    password,
+    category_id = None):
+    
+    if credential.vault_id != session.vault.vault_id:
+        raise ValueError("Credential does not belong to this vault")
+    
+    db.add(credential)
+    
+    orginal_username, orginal_password = decrypt_credential(
+        credential,
+        session
+    )
+        
+    
+    credential.category_id = category_id
+    credential.title = title.strip()
+    credential.website = website.strip() if website else None
+
+    
+    if orginal_username != username:
+        new_encrypted_username, new_username_nonce = encrypt_data(
+            username,
+            session.vault_key
+        )
+        credential.encrypted_username = new_encrypted_username
+        credential.username_nonce = new_username_nonce
+    
+    if orginal_password != password:
+        new_encrypted_password, new_password_nonce = encrypt_data(
+            password,
+            session.vault_key
+        )
+        credential.encrypted_password = new_encrypted_password
+        credential.password_nonce = new_password_nonce
+
+    db.commit()
+    db.refresh(credential)
+    db.expunge(credential)
+    
+    return credential
