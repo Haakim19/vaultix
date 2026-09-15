f7cf36fd8384f06d5c8234759d07ddb5c7e0cf02
Haakim19 <haakimajmal7@gmail.com>
2026-09-15 13:17:36 +0530

    lock button function added

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -6,6 +6,20 @@ def dashboard_window(session):
     if window is None:
         raise RuntimeError("Faild to load: 'ui/dashboard.ui'")
     
+    window.session = session
     window.vaultLabel.setText(session.vault.name)
     
+    window.lockVault.clicked.connect(
+        lambda: lock_vault(window)
+    )
     return window
+
+def lock_vault(window):
+    from app.views.login import login_window
+    
+    window.session = None
+    
+    window.login = login_window()
+    window.login.show()
+    
+    window.close()
