b9d05f196df8aad36ef03312e5bb1bfb9fe714ea
Haakim19 <haakimajmal7@gmail.com>
2026-09-21 12:33:21 +0530

    auto lock functions added and working

---
+++ app/utils/auto_lock.py
@@ -0,0 +1,38 @@
+from PySide6.QtCore import QTimer, QObject, QEvent
+
+class ActivityFilter(QObject):
+    def __init__(self, window):
+        super().__init__(window)
+        self.window = window
+    
+    def eventFilter(self, obj, event):
+        if event.type() in (
+            QEvent.MouseButtonPress,
+            QEvent.KeyPress,
+        ):
+            reset_auto_lock_timer(self.window)
+        
+        return False
+def remove_activity_filter(window):
+    from PySide6.QtWidgets import QApplication
+    
+    app = QApplication.instance()
+    
+    if app is not None and hasattr(window, "activity_filter"):
+        app.removeEventFilter(window.activity_filter)
+
+def create_auto_lock_timer(window, timeout = 5 * 60 * 1000):
+    timer = QTimer(window)
+    timer.setInterval(timeout)
+    
+    window.auto_lock_timer = timer
+    
+    return timer
+
+def reset_auto_lock_timer(window):
+    if hasattr(window, "activity_filter"):
+        window.auto_lock_timer.start()
+        print(
+            f"auto lock timer reset: "
+            f"{window.auto_lock_timer.remainingTime()} ms remaining."
+        )
