17821efb4e7e209990a534ff84802ac5f84cf1cc
Haakim19 <haakimajmal7@gmail.com>
2026-09-22 11:13:04 +0530

    load current & all other category into the combo box in edit window

--- app/views/credential_edit.py
+++ app/views/credential_edit.py
@@ -2,7 +2,26 @@ from app.utils.ui_loader import load_ui, show_message
 from app.utils.password_toggle import wire_password_toggle
 from app.services.credential_service import decrypt_credential
 from app.services.credential_service import update_credential
+from app.services.category_services import get_categories
 from app.database.database import SessionLocal
+
+def load_category(window):
+    with SessionLocal() as db:
+        categories = get_categories(
+            db,
+            window.session
+        )
+    window.categoryInput.clear()
+    
+    window.categoryInput.addItem(
+        "No category",
+        None
+    )
+    for category in categories:
+        window.categoryInput.addItem(
+            category.name,
+            category.category_id
+        )
 
 
 def credential_edit_window(session, credential):
@@ -14,6 +33,15 @@ def credential_edit_window(session, credential):
         )
 
     window.session = session
+    
+    load_category(window)
+    if credential.category_id is not None:
+        index = window.categoryInput.findData(
+            credential.category_id
+        )
+        if index != -1:
+            window.categoryInput.setCurrentIndex(index)
+    
     window.credential = credential
 
     username, password, notes = decrypt_credential(
@@ -44,6 +72,7 @@ def credential_edit_window(session, credential):
 
 
 def save_credential_changes( window, credential):
+    
     title = window.titleInput.text().strip()
     website = window.websiteInput.text().strip()
     username = window.usernameInput.text()
