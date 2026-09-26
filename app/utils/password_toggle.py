2847a269e7c9670c3116892f56d6cba185874125
Haakim19 <haakimajmal7@gmail.com>
2026-09-26 20:11:32 +0530

    password toggle button icon

--- app/utils/password_toggle.py
+++ app/utils/password_toggle.py
@@ -1,13 +1,15 @@
-from PySide6.QtWidgets import QLineEdit, QToolButton
+from pathlib import Path
+
+from PySide6.QtWidgets import  QLineEdit, QToolButton
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QIcon
 
-
-ICON_SHOW = "👁"
-ICON_HIDE = "🚫"
+ICON_SHOW = Path("ui/icons/Show.svg")
+ICON_HIDE = Path("ui/icons/Hide.svg")
 
 
 def wire_password_toggle(line_edit: QLineEdit, button: QToolButton) -> None:
-    button.setText(ICON_SHOW)
+    button.setIcon(QIcon(str(ICON_SHOW)))
     button.setCursor(Qt.PointingHandCursor)
     button.setToolTip("Show password")
     button.setFixedWidth(38)
@@ -15,11 +17,11 @@ def wire_password_toggle(line_edit: QLineEdit, button: QToolButton) -> None:
     def toggle():
         if line_edit.echoMode() == QLineEdit.Password:
             line_edit.setEchoMode(QLineEdit.Normal)
-            button.setText(ICON_HIDE)
+            button.setIcon(QIcon(str(ICON_HIDE)))
             button.setToolTip("Hide password")
         else:
             line_edit.setEchoMode(QLineEdit.Password)
-            button.setText(ICON_SHOW)
+            button.setIcon(QIcon(str(ICON_SHOW)))
             button.setToolTip("Show password")
 
     button.clicked.connect(toggle)
