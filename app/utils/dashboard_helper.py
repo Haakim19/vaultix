c41dd3fda6bbab3ae7e5466365f37ca255568cc3
Haakim19 <haakimajmal7@gmail.com>
2026-09-26 09:38:42 +0530

    load categories for combo box helpet function added

--- app/utils/dashboard_helper.py
+++ app/utils/dashboard_helper.py
@@ -1,6 +1,7 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QListWidgetItem
-
+from app.database.database import SessionLocal
+from app.services.category_services import get_categories
 def display_credentials(window, credentials):
     window.credentialList.clear()
 
@@ -8,3 +9,24 @@ def display_credentials(window, credentials):
         item = QListWidgetItem(credential.title)
         item.setData(Qt.UserRole, credential)
         window.credentialList.addItem(item)
+
+
+
+def load_categories(window):
+    with SessionLocal() as db:
+        categories = get_categories(
+            db,
+            window.session
+        )
+    window.categoryInput.clear()
+    
+    window.categoryInput.addItem(
+        "No category",
+        None
+    )
+    
+    for category in categories:
+        window.categoryInput.addItem(
+            category.name,
+            category.category_id
+        )
